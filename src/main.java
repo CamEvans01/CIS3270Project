6a33303f03a828635c75452a040cd552480194bb
Cameron Evans <camevans1357@gmail.com>
2018-03-25 21:34:56 -0400

    Hello World 3 times

--- src/main.java
+++ src/main.java
@@ -2,7 +2,12 @@
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		int x = 0;
+		while(x < 3) {
+			System.out.println("Hello World");
+			x++;
+		}
+		
 
 	}
 
