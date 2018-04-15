0cffa0849ed1d27b7f762d57751b031ecf6dc786
Cameron Evans <camevans1357@gmail.com>
2018-04-15 02:10:27 -0400

    GUI 90% done. Will add seperate return to customer and admin + links

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -13,7 +13,7 @@ import javax.swing.SwingConstants;
 
 public class MainMenu {
 
-	private JFrame frame;
+	JFrame frame;
 
 	/**
 	 * Launch the application.
@@ -46,35 +46,35 @@ public class MainMenu {
 		frame.setBounds(100, 100, 600, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel lblWelcome = new JLabel("Welcome");
 		lblWelcome.setBounds(279, 6, 90, 16);
 		frame.getContentPane().add(lblWelcome);
-		
+
 		JLabel lblNewLabel = new JLabel("Username");
 		lblNewLabel.setBounds(87, 142, 75, 16);
 		frame.getContentPane().add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Password");
 		lblNewLabel_1.setBounds(87, 228, 61, 16);
 		frame.getContentPane().add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Admin");
 		lblNewLabel_2.setBounds(98, 78, 75, 16);
 		frame.getContentPane().add(lblNewLabel_2);
-		
+
 		JLabel lblNewLabel_3 = new JLabel("Customer");
 		lblNewLabel_3.setBounds(449, 78, 61, 16);
 		frame.getContentPane().add(lblNewLabel_3);
-		
+
 		JLabel lblNewLabel_4 = new JLabel("Username");
 		lblNewLabel_4.setBounds(449, 142, 83, 16);
 		frame.getContentPane().add(lblNewLabel_4);
-		
+
 		JLabel lblNewLabel_5 = new JLabel("Password");
 		lblNewLabel_5.setBounds(449, 228, 61, 16);
 		frame.getContentPane().add(lblNewLabel_5);
-		
+
 		JButton btnAdminLogin = new JButton("Login");
 		btnAdminLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -82,15 +82,20 @@ public class MainMenu {
 		});
 		btnAdminLogin.setBounds(62, 330, 117, 29);
 		frame.getContentPane().add(btnAdminLogin);
-		
+
 		JButton btnAdminRegister = new JButton("Register");
 		btnAdminRegister.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+
+				Register_Admin fr = new Register_Admin();
+				fr.newScreen();
+				frame.dispose();
+
 			}
 		});
 		btnAdminRegister.setBounds(62, 400, 117, 29);
 		frame.getContentPane().add(btnAdminRegister);
-		
+
 		JButton btnCustomerLogin = new JButton("Login");
 		btnCustomerLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -98,37 +103,52 @@ public class MainMenu {
 		});
 		btnCustomerLogin.setBounds(426, 330, 117, 29);
 		frame.getContentPane().add(btnCustomerLogin);
-		
+
 		JButton btnCustomerRegister = new JButton("Register");
 		btnCustomerRegister.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Register_Customer fr = new Register_Customer();
+				fr.newScreen();
+				frame.dispose();
 			}
 		});
 		btnCustomerRegister.setBounds(426, 400, 117, 29);
 		frame.getContentPane().add(btnCustomerRegister);
-		
+
 		JTextPane textPane = new JTextPane();
 		textPane.setBounds(62, 270, 117, 16);
 		frame.getContentPane().add(textPane);
-		
+
 		JFormattedTextField formattedTextField = new JFormattedTextField();
 		formattedTextField.setBounds(62, 178, 117, 26);
 		frame.getContentPane().add(formattedTextField);
-		
+
 		JFormattedTextField formattedTextField_1 = new JFormattedTextField();
 		formattedTextField_1.setBounds(426, 170, 117, 26);
 		frame.getContentPane().add(formattedTextField_1);
-		
+
 		JFormattedTextField formattedTextField_2 = new JFormattedTextField();
 		formattedTextField_2.setBounds(426, 270, 117, 26);
 		frame.getContentPane().add(formattedTextField_2);
-		
+
 		JSeparator separator = new JSeparator();
 		separator.setOrientation(SwingConstants.VERTICAL);
 		separator.setBackground(Color.BLACK);
 		separator.setForeground(Color.BLACK);
 		separator.setBounds(301, 78, 10, 351);
 		frame.getContentPane().add(separator);
-		frame.setVisible(true);
+
+		JButton btnForgot_Password = new JButton("Forgot Password");
+		btnForgot_Password.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Forgot_Password fr = new Forgot_Password();
+				fr.newScreen();
+				frame.dispose();
+			}
+		});
+		btnForgot_Password.setBounds(240, 473, 129, 29);
+		frame.getContentPane().add(btnForgot_Password);
+
 	}
+
 }
